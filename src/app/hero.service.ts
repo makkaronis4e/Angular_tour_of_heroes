bb7b99b98ccbf73031f035f2536e3e0b07f6ff18
makkaronis4e <makkaronis4e@gmail.com>
2018-04-23 22:09:56 +0300

    Implement HttpClient into heroesService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,27 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
-  
+
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+
+  constructor(private http: HttpClient,
+    private messageService: MessageService) { }
+
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    // Todo: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
-  constructor(private messageService: MessageService) { }
 
-  getHero(id: number): Observable<Hero> {
-    // Todo: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
   }
 }
