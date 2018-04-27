863a957a87ef5b02326929cb8396506fb7cdc25b
makkaronis4e <makkaronis4e@gmail.com>
2018-04-27 21:43:34 +0300

    Add getHero func

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -19,11 +19,12 @@ export class HeroService {
     private messageService: MessageService) { }
 
   /** GET heroes from the server */
-  getHeroes(): Observable<Hero[]> {
+  getHeroes (): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
-    .pipe(
-      catchError(this.handleError('getHeroes', []))
-    );
+      .pipe(
+        tap(heroes => this.log(`fetched heroes`)),
+        catchError(this.handleError('getHeroes', []))
+      );
   }
   
   /**
@@ -50,4 +51,13 @@ export class HeroService {
   private log(message: string) {
     this.messageService.add('HeroService: ' + message);
   }
+
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
 }
+
