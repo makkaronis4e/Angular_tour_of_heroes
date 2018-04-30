cec7494c76ce295f41dfff8f3a9fcd453a55e142
makkaronis4e <makkaronis4e@gmail.com>
2018-04-30 21:42:38 +0300

    Add addHero option

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,7 +15,7 @@ const httpOptions = {
 
 @Injectable()
 export class HeroService {
-  
+
   private heroesUrl = 'api/heroes';  // URL to web api
 
 
@@ -63,12 +63,20 @@ export class HeroService {
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
   }
- 
+
   /** PUT: update the hero on the server */
   updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
       tap(_ => this.log(`updated hero id=${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
     );
   }
 }
